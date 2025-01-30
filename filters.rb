ff0df1c05bbc93f59e1d9396e0c26a889d1d67b3
Nesss86 <Nessymonster86@gmail.com>
2025-01-29 22:26:05 -0500

    Added methods

--- filters.rb
+++ filters.rb
@@ -1,16 +1,42 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
-    # Your code Here
+  def find(id)
+    @candidates.find { |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
+  def has_enough_github_points?(candidate)
+    candidate[:github_points] >= 100
+  end
+
+  def knows_required_languages?(candidate)
+    candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+  end
+
+  def applied_recently?(candidate)
+    candidate[:date_applied] >= 15.days.ago.to_date
+  end
+
+  def old_enough?(candidate)
+    candidate[:age] > 17
+  end
+
   def qualified_candidates(candidates)
-    # Your code Here
+    @candidates.select do |candidate|
+      experienced?(candidate) &&
+      has_enough_github_points?(candidate) &&
+      knows_required_languages?(candidate) &&
+      applied_recently?(candidate) &&
+      old_enough?(candidate)
+    end
   end
   
-  # More methods will go below
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by do |candidate|
+      [-candidate[:years_of_experience], -candidate[:github_points]]
+    end
+  end
--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
 # This is the main entrypoint into the program
 # It requires the other files/gems that it needs
 require 'pp'
-require './candidates'
-require './filters'
+require_relative 'candidates'
+require_relative 'filters'
 
-## Your test code can go here
 
-pp @candidates
+pp ordered_by_qualifications(@candidates)
